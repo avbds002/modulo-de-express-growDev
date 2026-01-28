08f0b3b87ab9a2d90ad43a92c9c8db7a364533f0
avbds002 <avbds2077@gmail.com>
2026-01-28 16:15:05 -0300

    Hello world com Express

--- src/index.js
+++ src/index.js
@@ -7,6 +7,25 @@ app.use(express.json());
 
 //Rotas
 
+// GET http://localhost:3000/teste
+app.get("/teste", (req, res) => {
+  res.send({
+    ok: true,
+    mensagem: "Teste da api realizado com sucesso",
+  });
+});
+
+// GET http://localhost:3000/about
+app.get("/about", (req, res) => {
+  res.send({
+    nome: "André Vinicius Bezerra da Silva",
+    email: "avbds2077@outlook.com",
+    resumo: "Programador back-end iniciante buscando vagas de estágio",
+    idade: 25,
+    skills: ["React", "NodeJs/Express", "Spring boot web", "JPA", "PostgreSQL"],
+  });
+});
+
 const port = process.env.PORT;
 
 app.listen(port, () => {
