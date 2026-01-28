5f8c86a5aca08f6f53aceb7ad41f5ac852b3cfda
avbds002 <avbds2077@gmail.com>
2026-01-28 18:46:18 -0300

    Cadastro de recurso (POST)

--- src/dados.js
+++ src/dados.js
@@ -1,16 +1,39 @@
+import { randomUUID } from "crypto";
+
 export const growDevers = [
   {
-    id: 1,
+    id: randomUUID(),
     nome: "Fred",
     email: "fred@scoobydoo.com",
     idade: 20,
     matriculado: true,
   },
   {
-    id: 2,
+    id: randomUUID(),
     nome: "Johnson",
-    email: "jh@us.com",
+    email: "jh@us.en.com",
     idade: 30,
+    matriculado: false,
+  },
+  {
+    id: randomUUID(),
+    nome: "Philip",
+    email: "philip@us.en.com",
+    idade: 19,
+    matriculado: true,
+  },
+  {
+    id: randomUUID(),
+    nome: "Nilson Napumoceno",
+    email: "nn@yahoo.com",
+    idade: 45,
+    matriculado: true,
+  },
+  {
+    id: randomUUID(),
+    nome: "Antoine Marie Cury",
+    email: "amc@montreaux.fr.com",
+    idade: 51,
     matriculado: false,
   },
 ];
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import { growDevers } from "./dados.js";
+import { randomUUID } from "crypto";
 dotenv.config();
 
 const app = express();
@@ -12,6 +13,27 @@ app.get("/growdevers", (req, res) => {
   res.status(200).send({
     ok: true,
     mensagem: "Growdevers listados com sucesso",
+    dados: growDevers,
+  });
+});
+
+//POST /growdevers
+app.post("/growdevers", (req, res) => {
+  const body = req.body;
+
+  const novoGrowDever = {
+    id: randomUUID(),
+    nome: body.nome,
+    email: body.email,
+    idade: body.idade,
+    matriculado: body.matriculado,
+  };
+
+  growDevers.push(novoGrowDever);
+
+  res.status(201).send({
+    ok: true,
+    mensagem: "Growdever criado com sucesso",
     dados: growDevers,
   });
 });
